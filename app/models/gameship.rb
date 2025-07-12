46babebec23d8fa22c56e36d87cf47b1b025a742
Alvin Crespo <alvin.crespo@gmail.com>
2025-07-12 06:49:59 -0400

    enable joining gameships on guildships directly

--- app/models/gameship.rb
+++ app/models/gameship.rb
@@ -2,4 +2,5 @@ class Gameship < ApplicationRecord
   belongs_to :game
   belongs_to :achievement_category
   belongs_to :guild
+  has_many :guildships, foreign_key: :guild_id, primary_key: :guild_id
 end
